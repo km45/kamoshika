75f5f40a5bb079b3b2736bb25eb6a5bee089ecbb
km45 <km45@users.noreply.github.com>
2018-08-05 17:15:17 +0900

    Refactor dump.py

--- kamoshika/dump.py
+++ kamoshika/dump.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 import os.path
+import typing
 
 
-def dump_files(src: dict, dst: str)->None:
-    for key, value in src.items():
-        filename = os.path.join(dst, key)
-        dirname = os.path.dirname(filename)
-        if not os.path.exists(dirname):
-            os.makedirs(dirname)
-        with open(filename, "wb") as out:
-            out.write(value)
+def dump_files(src: typing.Dict[str, bytes], dst: str) -> None:
+    for src_key, from_bytes in src.items():
+        to_filename = os.path.join(dst, src_key)
+        to_dirname = os.path.dirname(to_filename)
+        if not os.path.exists(to_dirname):
+            os.makedirs(to_dirname)
+        with open(to_filename, 'wb') as to_file:
+            to_file.write(from_bytes)
