29c96b34ed3477b229be57c047c06f264fe6e3ea
km45 <km45@users.noreply.github.com>
2018-10-28 16:54:11 +0900

    Bump version

--- kamoshika/version.py
+++ kamoshika/version.py
@@ -2,6 +2,6 @@
 """Define version"""
 _MAJOR_VERSION = 0
 _MINOR_VERSION = 10
-_PATCH_VERSION = 0
+_PATCH_VERSION = 1
 
 __version__ = '{}.{}.{}'.format(_MAJOR_VERSION, _MINOR_VERSION, _PATCH_VERSION)
